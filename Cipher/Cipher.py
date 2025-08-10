0bbf5b80239ab55356f9ca8c3441b29ccd49c209
CodeByRasul <rasuldjalgasov2@gmail.com>
2025-08-10 22:28:51 +0500

    rosetta

--- Cipher/Cipher.py
+++ Cipher/Cipher.py
@@ -1,9 +1,21 @@
-text = 'Hello World'
-shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+text = 'Hello Zaira'
+custom_key = 'python'
 
-for char in text.lower():
-    index = alphabet.find(char)
-    new_index = index + shift
-    new_char = alphabet[new_index]
-    print('char:', char, 'new char:', new_char)
+def vigenere(message, key):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:
+            
+            key_char = key[key_index % len(key)]
+            key_index += 1
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    print('plain text:', message)
+    print('encrypted text:', encrypted_text)
